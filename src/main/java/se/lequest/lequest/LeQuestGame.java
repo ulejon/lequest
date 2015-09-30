62688b8673948878c1a9fbd4cf34afd3946118e0
ulrikl <ulrik.lejon@gmail.com>
2015-09-30 14:26:00 +0200

    Added logback for logging and updated zip creation to include dependencies

--- src/main/java/se/lequest/lequest/LeQuestGame.java
+++ src/main/java/se/lequest/lequest/LeQuestGame.java
@@ -4,21 +4,25 @@ import javax.swing.*;
 
 import se.lequest.lequest.fileio.FileIO;
 import se.lequest.lequest.gui.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LeQuestGame {
+    private static final Logger LOG = LoggerFactory.getLogger(LeQuestGame.class);
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
+        LOG.trace("Starting game");
 
-        try{	//Makes the GUI look more "Windows"-like.
+        try {
+            //Makes the GUI look more "Windows"-like.
             //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
             UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception e) {
+            LOG.warn("Unable to use desired look and feel");
         }
-        catch (Exception e ) {}
+
         FileIO.loadHighScore();
-        IntroFrame i = new IntroFrame();
+        new IntroFrame();
     }
 
 }
